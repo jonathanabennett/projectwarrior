d3b78c4fc3d8c60b73fd760d566d1a5559d071cd
Jonathan A. Bennett <doulos05@gmail.com>
2021-06-21 17:09:01 +0900

    Expose new API Elements.

--- package.lisp
+++ package.lisp
@@ -4,4 +4,8 @@
   (:use #:cl)
   (:export #:main
            #:merge-lists
+           #:ask
+           #:get-new-projects-list
+           #:sync-projects-list
+           #:*projects-filepath*
            #:get-list-from-file))
