4e8b7cee070f6e317db2bcd8f1a47106371e84e7
Jonathan Bennett <doulos05@gmail.com>
2022-01-05 20:56:20 +0900

    Project Creation
    Can now create projects in memory and append them to a project list.

--- project.lisp
+++ project.lisp
@@ -13,17 +13,6 @@
     :initarg :description
     :accessor description
     :documentation "A sentence description of a project.")
-   (start-date
-    :initarg :start-date
-    :initform nil
-    :accessor start-date
-    :documentation "The date this was first entered into gtd-review. Stored as a date object.")
-   (last-review
-    :initarg :last-review
-    :initform nil
-    :initform nil
-    :accessor last-review
-    :documentation "The datetime that this was last reviewed. Stored as a datetime object.")
    (slug
     :initarg :slug
     :initform ""
@@ -39,11 +28,21 @@
     :initform ""
     :accessor area-of-focus
     :documentation "The Area of Focus this project is tied to.")
-   (deadline
-    :initarg :deadline
-    :initform nil
-    :accessor deadline
-    :documentation "The date after which this project can be considered late.")
+   ;; (start-date
+   ;;  :initarg :start-date
+   ;;  :initform nil
+   ;;  :accessor start-date
+   ;;  :documentation "The date this was first entered into gtd-review. Stored as a date object.")
+   ;; (last-review
+   ;;  :initarg :last-review
+   ;;  :initform nil
+   ;;  :accessor last-review
+   ;;  :documentation "The datetime that this was last reviewed. Stored as a datetime object.")
+   ;; (deadline
+   ;;  :initarg :deadline
+   ;;  :initform nil
+   ;;  :accessor deadline
+   ;;  :documentation "The date after which this project can be considered late.")
    (inherit-tags
     :initarg :inherit-tags
     :initform '()
@@ -54,7 +53,27 @@
 (defun project-from-taskwarrior (name)
   (make-instance 'project
                  :description name
-                 :uuid (uuid::make-v5-uuid "gtd-review" name)))
+                 :slug name
+                 :uuid (uuid::make-v5-uuid uuid::+namespace-dns+ name)))
 
+;; Make project with optionals
+(defun make-project (&key uuid description slug tags inherit-tags)
+  (if (eq slug nil)
+      (setf slug (cl-slug::slugify description)))
+  (if (eq uuid nil)
+      (setf uuid (uuid::make-v5-uuid uuid::+namespace-dns+ slug)))
+  (make-instance 'project
+                 :description description
+                 :uuid uuid
+                 :slug slug
+                 :tags tags
+                 :inherit-tags inherit-tags))
+
+(defun add-project (p database)
+  (push p database))
+
+(defmethod display ((p project))
+  (format nil "~a: ~a" (description p) (slug p)))
+;; Save project to file as an alist
 (defmethod project-to-file (project p)
   "This function will write the project out to a file as a lisp form which, when evaluated, will create the project again.")
